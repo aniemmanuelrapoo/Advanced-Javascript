bb5d076b794e089a0cf400a7bf298087abdde176
Emmanuel rapoo <aniemmanuelrapo@gmail.com>
2022-04-12 02:17:50 +0100

    understanding aschronous

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-function a(){
-    function b() {
-        console.log(myvar)
-    }
-    b()
+// long running function
+function waitThreeSecounds() {
+    var ms = 3000 + new Date().getTime();
+    while (new Date() < ms){}
+    console.log('finished function')
 }
 
-var myvar = 1
-a();
+function clickHandler(){
+    console.log('click Event')
+}
+
+//listen for the click event
+document.addEventListener('click', clickHandler);
+
+waitThreeSecounds();
+console.log('finished execution')
