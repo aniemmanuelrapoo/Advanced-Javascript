3631c5526195a6d0b6127bb8dfcdd2eafddf8183
Emmanuel rapoo <aniemmanuelrapo@gmail.com>
2022-04-15 00:53:46 +0100

    operators

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
-// long running function
-function waitThreeSecounds() {
-    var ms = 3000 + new Date().getTime();
-    while (new Date() < ms){}
-    console.log('finished function')
-}
+// // long running function
+// function waitThreeSecounds() {
+//     var ms = 3000 + new Date().getTime();
+//     while (new Date() < ms){}
+//     console.log('finished function')
+// }
 
-function clickHandler(){
-    console.log('click Event')
-}
+// function clickHandler(){
+//     console.log('click Event')
+// }
 
-//listen for the click event
-document.addEventListener('click', clickHandler);
+// //listen for the click event
+// document.addEventListener('click', clickHandler);
 
-waitThreeSecounds();
-console.log('finished execution')
+// waitThreeSecounds();
+// console.log('finished execution')
 
-console.log( new Date())
+// console.log( new Date())
+
