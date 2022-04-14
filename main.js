4c27628b4e23cb0e850c077f51ea637bdb700c9e
Emmanuel rapoo <aniemmanuelrapo@gmail.com>
2022-04-14 11:46:53 +0100

    debugging error

---
+++ main.js
@@ -0,0 +1,18 @@
+// long running function
+function waitThreeSecounds() {
+    var ms = 3000 + new Date().getTime();
+    while (new Date() < ms){}
+    console.log('finished function')
+}
+
+function clickHandler(){
+    console.log('click Event')
+}
+
+//listen for the click event
+document.addEventListener('click', clickHandler);
+
+waitThreeSecounds();
+console.log('finished execution')
+
+console.log( new Date())
