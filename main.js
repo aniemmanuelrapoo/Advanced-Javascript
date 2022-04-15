ae6bea80c83738de1c961d58c820e5dd4ad4c863
Emmanuel rapoo <aniemmanuelrapo@gmail.com>
2022-04-15 01:20:33 +0100

    operators precidence

--- main.js
+++ main.js
@@ -17,6 +17,14 @@
 
 // console.log( new Date())
 
-var a = 3 + 4 * 5;
-console.log(a);
+// var a = 3 + 4 * 5;
+// console.log(a);
+
+var a = 2, b = 3, c = 4
+
+a = b = c
+
+console.log(a)
+console.log(b)
+console.log(c)
 
