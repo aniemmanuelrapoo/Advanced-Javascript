ce3aa7853c34aa18aeece160b3b01cd2aa3c5352
Emmanuel rapoo <aniemmanuelrapo@gmail.com>
2022-04-15 01:23:05 +0100

    operators precidence and assciativity

--- main.js
+++ main.js
@@ -20,13 +20,13 @@
 // var a = 3 + 4 * 5;
 // console.log(a);
 
-var a = 2, b = 3, c = 4
+// var a = 2, b = 3, c = 4
 
-a = b = c
+// a = b = c
 
-console.log(a)
-console.log(b)
-console.log(c)
+// console.log(a)
+// console.log(b)
+// console.log(c)
 
 var a = (3 + 4) * 5;
 console.log(a);
