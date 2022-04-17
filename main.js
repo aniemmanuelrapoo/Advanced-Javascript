59199952beb643d837c92a418c433f2a69467d87
emmanuel rapoo <aniemmanuelrapo@gmail.com>
2022-04-17 21:17:03 +0100

    Commit

--- main.js
+++ main.js
@@ -39,11 +39,11 @@ comparisoin
 console.log(1 < 2 < 3)
 
 
-var a = 0
-var b = false
+// var a = 0
+// var b = false
 
-if(a === b){
-    console.log('The are equal')
-}else{
-    console.log('Nope, not equal')
-}
+// if(a === b){
+//     console.log('The are equal')
+// }else{
+//     console.log('Nope, not equal')
+// }
