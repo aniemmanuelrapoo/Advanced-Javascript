e57da0d7a375c00b644c925614a5612ff6e1eed7
emmanuel rapoo <aniemmanuelrapo@gmail.com>
2022-04-17 21:22:50 +0100

    debugger

--- main.js
+++ main.js
@@ -42,8 +42,8 @@ console.log(1 < 2 < 3)
 var a = 0
 var b = false
 
-// if(a === b){
-//     console.log('The are equal')
-// }else{
-//     console.log('Nope, not equal')
-// }
+if(a === b){
+    console.log('The are equal')
+}else{
+    console.log('Nope, not equal')
+}
