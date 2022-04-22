b6ce065d81f3c9a145231b913859340e78224f9e
emmanuel rapoo <aniemmanuelrapo@gmail.com>
2022-04-22 13:01:00 +0100

    Understanding Closures part 2

--- main2.js
+++ main2.js
@@ -1,9 +1,28 @@
-//Understanding Closures
-function greet(whattosay){
+// //Understanding Closures
+// function greet(whattosay){
 
-    return (name) => {
-        console.log(`${whattosay} ${name}`);
+//     return (name) => {
+//         console.log(`${whattosay} ${name}`);
+//     }
+// }
+// var sayHi = greet('Hi');
+// sayHi("Tony")
+
+//Understanding Closures part 2
+function buildFunction(){
+    var arr = [];
+    for (var i = 0; i < 3; i++){
+        arr.push(
+            function(){
+                console.log(i);
+            }
+        )
     }
+    return arr;
 }
-var sayHi = greet('Hi');
-sayHi("Tony")
+
+var fs = buildFunction();
+
+fs[0]();
+fs[1]();
+fs[2]();
