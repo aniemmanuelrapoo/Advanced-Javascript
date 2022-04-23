56db94a58446d363d4be51157dec0544553786ce
emmanuel rapoo <aniemmanuelrapo@gmail.com>
2022-04-23 04:33:15 +0100

    function factories (closures)

--- main2.js
+++ main2.js
@@ -8,53 +8,71 @@
 // var sayHi = greet('Hi');
 // sayHi("Tony")
 
-//Understanding Closures part 2 example1
-function buildFunction(){
-    var arr = [];
-    for (var i = 0; i < 3; i++){
-        arr.push(
-            function(){
-                console.log(i);
-            }
-        )
+// //Understanding Closures part 2 example1
+// function buildFunction(){
+//     var arr = [];
+//     for (var i = 0; i < 3; i++){
+//         arr.push(
+//             function(){
+//                 console.log(i);
+//             }
+//         )
+//     }
+//     return arr;
+// }
+
+// var fs = buildFunction();
+
+// fs[0]();
+// fs[1]();
+// fs[2]();
+
+// //Understanding Closures part 2 example2
+// function buildFunction2(){
+//     var arr = [];
+//     //es6 fmt
+//     // for (var i = 0; i < 3; i++){
+//     //     let j = i;
+//     //     arr.push(
+//     //         function(){
+//     //             console.log(j);
+//     //         }
+//     //     )
+//     // }
+
+//     //es5 fmt
+//     for (var i = 0; i < 3; i++){
+//         arr.push(
+//             (function(j){
+//                 return function(){
+//                     console.log(j);
+//                 }
+//             }(i))
+//         )
+//     }
+//     return arr;
+// }
+
+// var fs2 = buildFunction2();
+
+// fs2[0]();
+// fs2[1]();
+// fs2[2]();
+
+// function factories (closures)
+function makeGreeting(language){
+    return function(firstname, lastname){
+        if(language === 'en'){
+            console.log(`Hello ${firstname} ${lastname}`);
+        }
+        if(language === 'es'){
+            console.log(`Hola ${firstname} ${lastname}`);
+        }
     }
-    return arr;
 }
 
-var fs = buildFunction();
+var greetEnglish = makeGreeting('en')
+var greetSpanish = makeGreeting('es')
 
-fs[0]();
-fs[1]();
-fs[2]();
-
-//Understanding Closures part 2 example2
-function buildFunction2(){
-    var arr = [];
-    //es6 fmt
-    // for (var i = 0; i < 3; i++){
-    //     let j = i;
-    //     arr.push(
-    //         function(){
-    //             console.log(j);
-    //         }
-    //     )
-    // }
-
-    //es5 fmt
-    for (var i = 0; i < 3; i++){
-        arr.push(
-            (function(j){
-                return function(){
-                    console.log(j);
-                }
-            }(i))
-        )
-    }
-    return arr;
-}
-
-var fs2 = buildFunction2();
-
-fs2[0]();
-fs2[1]();
-fs2[2]();
+greetEnglish('john', 'doe')
+greetSpanish('rapo', 'man')
