495b71c78701a359b1797fb96d2dddfe45919c11
emmanuel rapoo <aniemmanuelrapo@gmail.com>
2022-04-24 13:47:32 +0100

    call apply and bind
    understanding bind

--- main2.js
+++ main2.js
@@ -90,32 +90,51 @@
 // markAge('mark', 'john')
 // lightAge('light', 'bro')
 
-//closeures and callbacks
-function sayHiLater(){
-    var greeting = "Hi";
+// //closeures and callbacks
+// function sayHiLater(){
+//     var greeting = "Hi";
 
-    setTimeout(() => {
-        console.log(greeting);
-    }, 3000);
+//     setTimeout(() => {
+//         console.log(greeting);
+//     }, 3000);
+// }
+
+// sayHiLater();
+
+// //jQuery uses function expression and first-class function
+// //$("button").click(function(){
+
+// // });
+
+// // callback function
+// function tellMeWhenDone(callback){
+//     var a = 1000; // some work
+//     var b = 2000; // some work
+
+//     callback(); //the 'callback', it runs the function i give it!
+// }
+// tellMeWhenDone(function(){
+//     console.log('I am done');
+// })
+// tellMeWhenDone(() => {
+//     console.log('I am done the secound time');
+// })
+
+//call apply and bind
+//understanding bind
+var person = {
+    firstname: 'john',
+    lastname: 'doe',
+    getFullname: function() {
+        let fullname = `${this.firstname} ${this.lastname}`
+        return fullname;
+    }
 }
 
-sayHiLater();
-
-//jQuery uses function expression and first-class function
-//$("button").click(function(){
-
-// });
-
-// callback function
-function tellMeWhenDone(callback){
-    var a = 1000; // some work
-    var b = 2000; // some work
-
-    callback(); //the 'callback', it runs the function i give it!
+var logName = function(lang1, lang2){
+    console.log(`Logged ${this.getFullname()}`);
+    console.log(`Argument ${lang1} ${lang2}`);
+    console.log('.................');
 }
-tellMeWhenDone(function(){
-    console.log('I am done');
-})
-tellMeWhenDone(() => {
-    console.log('I am done the secound time');
-})
+var logPersonName = logName.bind(person);
+logPersonName('en', 'es');
