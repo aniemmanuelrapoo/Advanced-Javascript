6c9ef64597439d1aa6a903af37e50c7764ab063e
emmanuel rapoo <aniemmanuelrapo@gmail.com>
2022-04-24 13:16:45 +0100

    understanding callback function

--- main2.js
+++ main2.js
@@ -105,3 +105,17 @@ sayHiLater();
 //$("button").click(function(){
 
 // });
+
+// callback function
+function tellMeWhenDone(callback){
+    var a = 1000; // some work
+    var b = 2000; // some work
+
+    callback(); //the 'callback', it runs the function i give it!
+}
+tellMeWhenDone(function(){
+    console.log('I am done');
+})
+tellMeWhenDone(() => {
+    console.log('I am done the secound time');
+})
