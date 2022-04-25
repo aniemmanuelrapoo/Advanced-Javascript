5782ca33ea52cc4bef3f5494f42c1e50ce49f46a
emmanuel rapoo <aniemmanuelrapo@gmail.com>
2022-04-25 01:37:45 +0100

    Reflection and extend

--- main3.js
+++ main3.js
@@ -24,7 +24,47 @@
 // console.log(jane.getFullName());
 
 
-//Everything is an object or a primitive
-var a = {};
-var b = function() { };
-var c = [];
+// //Everything is an object or a primitive
+// var a = {};
+// var b = function() { };
+// var c = [];
+
+//Reflection and extend
+//understanding reflection
+var person = {
+    fisrtname: 'Default',
+    lastname: 'Default',
+    getFullName: function(){
+        return `${this.fisrtname} ${this.lastname}`
+    }
+}
+
+var john = {
+    fisrtname: 'John',
+    lastname: 'Doe'
+}
+
+//dont do this EVER! for demo purposes only!!
+john.__proto__ = person
+
+for(var prop in john){
+    if(john.hasOwnProperty(prop)){
+        console.log(prop + ': ' + john[prop]);
+    }
+}
+
+//using underscore
+var jane = {
+    address: '111 main st',
+    getFormalFullName: function(){
+        return this.lastname + ", " + this.fisrtname;
+    }
+}
+
+var jim = {
+    getFirstName: function(){
+        return fisrtname;
+    }
+}
+_.extend(john, jane, jim);
+console.log(john);
