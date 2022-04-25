35b561670271a2b5456e3870adcb15b2c76b2284
emmanuel rapoo <aniemmanuelrapo@gmail.com>
2022-04-25 01:09:19 +0100

    Everything is an object or a primitive

--- main3.js
+++ main3.js
@@ -1,24 +1,30 @@
-//understanding prototype
-var person = {
-    fisrtname: 'Default',
-    lastname: 'Default',
-    getFullName: function(){
-        return `${this.fisrtname} ${this.lastname}`
-    }
-}
+// //understanding prototype
+// var person = {
+//     fisrtname: 'Default',
+//     lastname: 'Default',
+//     getFullName: function(){
+//         return `${this.fisrtname} ${this.lastname}`
+//     }
+// }
 
-var john = {
-    fisrtname: 'John',
-    lastname: 'Doe'
-}
+// var john = {
+//     fisrtname: 'John',
+//     lastname: 'Doe'
+// }
 
-//dont do this EVER! for demo purposes only!!
-john.__proto__ = person
-console.log(john.getFullName());
-console.log(john.fisrtname);
+// //dont do this EVER! for demo purposes only!!
+// john.__proto__ = person
+// console.log(john.getFullName());
+// console.log(john.fisrtname);
 
-var jane = {
-    fisrtname: 'Jane'
-}
-jane.__proto__ = person;
-console.log(jane.getFullName());
+// var jane = {
+//     fisrtname: 'Jane'
+// }
+// jane.__proto__ = person;
+// console.log(jane.getFullName());
+
+
+//Everything is an object or a primitive
+var a = {};
+var b = function() { };
+var c = [];
