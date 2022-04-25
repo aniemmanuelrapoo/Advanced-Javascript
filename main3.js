00ca45d45141c0a13106f2085d569f3bc5428de2
emmanuel rapoo <aniemmanuelrapo@gmail.com>
2022-04-25 01:01:08 +0100

    understanding prototype
    Never use this pattern

---
+++ main3.js
@@ -0,0 +1,24 @@
+//understanding prototype
+var person = {
+    fisrtname: 'Default',
+    lastname: 'Default',
+    getFullName: function(){
+        return `${this.fisrtname} ${this.lastname}`
+    }
+}
+
+var john = {
+    fisrtname: 'John',
+    lastname: 'Doe'
+}
+
+//dont do this EVER! for demo purposes only!!
+john.__proto__ = person
+console.log(john.getFullName());
+console.log(john.fisrtname);
+
+var jane = {
+    fisrtname: 'Jane'
+}
+jane.__proto__ = person;
+console.log(jane.getFullName());
